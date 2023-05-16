733f2c6fdf3448e635bca5afa80a892e7d07be0d
Saji Shunnarah <sajishunnarah@gmail.com>
2023-05-16 07:50:16 -0400

    installed everything

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    temperature: 0,
-    max_tokens: 7,
-});
+
+const getResponse = async () => {
+    return await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        temperature: 0,
+        max_tokens: 7,
+    });
+}
+const response = getResponse();
+console.log(response.data);
