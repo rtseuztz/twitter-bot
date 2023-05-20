c1837921a04a5d4ed3276aded5f21c80756e1db0
Saji Shunnarah <sajishunnarah@gmail.com>
2023-05-20 17:16:16 -0400

    added more tests

--- tweet_generator.js
+++ tweet_generator.js
@@ -8,7 +8,8 @@ const getResponse = async () => {
     });
     const openai = new OpenAIApi(configuration);
     try {
-        console.log(process.env.TWITTER_USERNAME)
+        console.log(process.env.TWITTER_USERNAME.split(" "))
+        console.log(process.env.TEST)
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: "imagine you are a twitter user. you are named boolman6 and are slightly edgy sometimes. you have a lot of viral tweets that get requests for ads in the replies. this user, which is you, is a 22 year old that has a lot of different opinions which people react to. you do not use hashtags and you type in all lowercase, yet sometimes you capitalize words. you do not use the # character. generate another tweet that goes viral. an example of a tweet that went viral is: \"didnt know a forehead could be so big until i saw charles barkely's.\" they need to be very random and not follow the example. generate it without quotes.",
